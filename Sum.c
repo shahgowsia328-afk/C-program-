b5e723491939fa1d0fa23ec09bad2e682303ae12
shahgowsia328-afk <shahgowsia328@gmail.com>
2025-11-25 17:28:46 +0530

    Create Sum.c

---
+++ Sum.c
@@ -0,0 +1,11 @@
+//
+#include <stdio.h>
+int main ()
+{
+    int x,y,z;
+    printf("enter the numbers to be added\n");
+    scanf(" %d %d" , &x,&y);
+    z=x+y;
+    printf("the sum is %d", z);
+   return 0;
+}
